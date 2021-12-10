d47cfa55f018259897cbbeb0d09c9092ff1c0bbf
Ediz Yurdakul <edizyurdakul@gmail.com>
2021-12-10 07:46:57 +0300

    day 1 part two

--- day01/index.js
+++ day01/index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 
+//
 const input = fs
   .readFileSync("input.txt", { encoding: "utf-8" })
   .split("\n")
@@ -22,4 +23,29 @@ console.log(increased);
 195 - current
 199
 218
+*/
+
+// part two
+
+increased = 0;
+
+for (let i = 3; i < input.length; i++) {
+  const current = input[i] + input[i - 1] + input[i - 2];
+  const last = input[i - 1] + input[i - 2] + input[i - 3];
+  current > last ? increased++ : null;
+}
+
+console.log(increased);
+
+/* 
+199  A      
+200  A B    
+208  A B C  
+210    B C D
+200  E   C D
+207  E F   D
+240  E F G  
+269    F G H
+260      G H
+263        H
 */
